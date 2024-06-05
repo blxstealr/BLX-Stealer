01ceacf21ead3b44571dd18be5c2c2f59c531354
blx in the dark <162599336+blxstealr@users.noreply.github.com>
2024-06-05 12:08:52 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -6,12 +6,10 @@ pip install auto_py_to_exe
 pip install pyinstaller
 pip install wmi
 pip install pyOpenSSL
-pip install OpenSSL
 pip install get-mac
 pip install psutil
-pip install ctypes
-pip install Crypto.Cipher
 pip install fade
 pip install discord
 pip install browser_cookie3
 pip install discord_webhook
+pip install customtkinter
